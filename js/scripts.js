42233af9677e281135d771d9b920ebdc2a94ca9c
maxandrewbeale <maxandrewbeale@gmail.com>
2019-06-15 16:11:17 +0100

    add jQuery to animate nav when burger bar is clicked, rearange links in the head

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,29 @@
-/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
-function myFunction() {
-  var x = document.getElementById("myLinks");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-} 
+// /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+// function myFunction() {
+//   var x = document.getElementById("myLinks");
+//   if (x.style.display === "block") {
+//     x.style.display = "none";
+//   } else {
+//     x.style.display = "block";
+//   }
+// }
+var y = true;
+
+$("#myLinks").hide();
+$("#burger").click(function() {
+    if (y) {
+    $("#myLinks").slideDown(200);
+    y = false;
+    } else {
+    $("#myLinks").slideUp(300);
+    y = true;
+    }
+});
+
+
+
+// $("#button").click(function() {
+//     $([document.documentElement, document.body]).animate({
+//         scrollTop: $("#elementtoScrollToID").offset().top
+//     }, 2000);
+// });
